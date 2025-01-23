7c30f05d2ff14ef8fbda44cf6c784c2b2ff4dffe
XOKEESH <hello@kishamartin.com>
2025-01-22 23:56:47 -0800

    updates to intro statements

--- src/components/PersonasIntro.jsx
+++ src/components/PersonasIntro.jsx
@@ -6,11 +6,11 @@ const PersonasIntro = () => {
 
   const intros = {
     forAnyone: "Hello there! I'm a designer and engineer who's passionate about creating thoughtful, user-centered solutions that make life easier.",
-    recruiters: "I'm a Designer and Engineer with 10+ years of cross-functional experience, combining design expertise and technical skills to deliver solutions that drive user satisfaction and business impact.",
+    recruiters: "With 10+ years of cross-functional experience, I combine design expertise and technical skills to deliver solutions that drive user satisfaction and business impact.",
     designDirectors: "I thrive in complex, ambiguous environments where collaboration and adaptability are key. My diverse background allows me to bridge perspectives and ensure every solution is both thoughtful and functional.",
     productDesigners: "I'm a meticulous designer who thrives on collaboration, creating seamless, meaningful experiences that solve real problems. I'll collaborate with you, learn from you, and help make something we're proud of.",
     productManagers: "I bring a user-focused mindset and a deep understanding of both design and development, connecting strategy and execution to deliver solutions that work for users and businesses alike.",
-    engineers: "I'm a results-driven engineer who excels in solving complex challenges, creating scalable, high-quality solutions that integrate seamlessly into the user experience."
+    engineers: "I'm a results-driven Engineer who excels in solving complex challenges, creating scalable, high-quality solutions that integrate seamlessly into the user experience."
   }
 
   return (
