b3e02590ca648152a6aaa915a2f5f98b3202b21b
XOKEESH <hello@kishamartin.com>
2025-01-23 21:02:18 -0800

    updates to values section

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import './Background.css'
 
 const Background = () => {
-    return (
-      <section id="background" className="section">
-        <p>I studied visual communication design, got my start in branding, marketing, and web design, then evolved to specialize in product design. Leveraging my visual design foundation and deep user empathy, I've established myself as a well rounded design leader who creates human-centered platforms with cohesive throughlines at every touchpoint: from vision and strategy, to brand awareness and marketing, to product development and delightful micro interactions. Throughout my 15 years of professional experience, I've worked freelance, in-house, at startups, and at established public companies, developing a wide range of multi-disciplinary skills in diverse contexts. My insatiable curiosity, high craft, and adaptability, enable me to generate impact in our ever-changing environment.</p>
-      </section>
-    )
-  }
-  
-  export default Background
+  return (
+    <section id="background" className="background">
+      <div className="background-left">
+        <p>
+          My career journey began in fashion design, where I launched my own brand and worked as a 
+          Creative Director for other brands. I even had the privilege of collaborating with big names 
+          and celebrities. Along the way, I honed my skills in leadership, communication, and customer 
+          experience through roles in management, sales, and customer service.
+        </p>
+      </div>
+      <div className="background-right">
+        <p>
+          In 2022, I earned a UX Design certificate from General Assembly, supported by a full 
+          scholarship from <span className="highlight">Microsoft</span>, which deepened my commitment to 
+          creating thoughtful, user-centered digital experiences. In 2024, I further expanded my technical expertise by earning a Software Engineering 
+          certificate from General Assembly, solidifying my ability to bridge design and development.
+        </p>
+        <p>
+          Now, I combine my design and engineering skills to craft innovative products that truly meet 
+          users' needs.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Background
 
 
